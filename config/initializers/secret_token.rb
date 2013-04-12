e375bee21c45937d9aad557d9f43fcf4c0387710
Tung Pham <timpham2003@gmail.com>
2013-04-11 22:33:11 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nutrientmanager::Application.config.secret_token = 'a4d18310d7ddcec45444cb5fadb31b69e9080c156e4f9ef14a6c4186468802651b16ca7d9b69debb8798bdfba7ca749a09d590ebca0890771b0ffc871246f920'
